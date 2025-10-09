b464d62205deace56a8aaf84c07346369aba88ee
raj-deshmukh6403 <152682957+raj-deshmukh6403@users.noreply.github.com>
2025-10-09 23:00:18 +0530

    test

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -305,7 +305,7 @@ const Footer = () => {
           </div>
         )}
 
-        {/* Bottom Bar */}
+        {/* Bottom Bar code */}
         <div className="border-t border-gray-700 mt-8 pt-6">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm mb-4 md:mb-0">
